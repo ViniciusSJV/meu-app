b3f6b76944b2dc61c9eaf53e8ecb680b16f60215
ViniciusSJV <beatles.stz25@gmail.com>
2018-05-17 22:26:06 -0300

    permitir logar com novos usuarios

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -25,18 +25,14 @@ export class LoginComponent implements OnInit {
         private loginService: LoginService,
         private formBuilder: FormBuilder) {
 
-          const usuarios: Usuario[] = [
-            {id: 1, nome: 'admin', usuario: 'admin', email: 'mail1@mail', senha: '123', celular: '997653456', admin: true},
-            {id: 2, nome: 'vinicius', usuario: 'vinicius', email: 'mail2@mail', senha: '123', celular: '998762345', admin: false},
-            {id: 3, nome: 'mariana', usuario: '3', email: 'mail3@mail', senha: '123', celular: '99807653', admin: false},
-            {id: 4, nome: 'marcos', usuario: '4', email: 'mail4@mail', senha: '123', celular: '998087656', admin: false},
-            {id: 5, nome: 'joao', usuario: '5', email: 'mail@mail', senha: '123', celular: '998527939', admin: false},
-            {id: 6, nome: 'paulo', usuario: '6', email: 'mail@mail', senha: '123', celular: '998527939', admin: false},
-            {id: 7, nome: 'souza', usuario: '7', email: 'mail@mail', senha: '123', celular: '998527939', admin: false},
-            {id: 8, nome: 'joao', usuario: '8', email: 'mail@mail', senha: '123', celular: '998527939', admin: false},
-            {id: 9, nome: 'vinicius', usuario: '9', email: 'mail@mail', senha: '123', celular: '998527939', admin: false},
-            {id: 10, nome: 'souza', usuario: '10', email: 'mail@mail', senha: '123', celular: '998527939', admin: false}
-          ];
+          let usuarios: Usuario[] = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+          if(usuarios.length === 0){
+            usuarios = [
+                {id: 1, nome: 'admin', usuario: 'admin', email: 'mail1@mail', senha: '123', celular: '997653456', admin: true},
+                {id: 2, nome: 'vinicius', usuario: 'vinicius', email: 'mail2@mail', senha: '123', celular: '998762345', admin: false}
+              ];
+          }
 
           localStorage.setItem('usuarios', JSON.stringify(usuarios));
 
